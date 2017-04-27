664794ed0da158c53979eab698476dee66aaac3a
Dan Iter <daniter@stanford.edu>
2017-04-27 13:58:21 -0700

    switched to c++ for library

--- src/compiler/fsintercept.cc
+++ src/compiler/fsintercept.cc
@@ -1,10 +1,9 @@
-#define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
 
 typedef int ( *orig_open_f_type )( const char *pathname, int flags );
  
-int open( const char *pathname, int flags, ... )
+extern "C" int open( const char *pathname, int flags, ... )
 {
   orig_open_f_type orig_open;
   orig_open = (orig_open_f_type)dlsym( RTLD_NEXT, "open" );
