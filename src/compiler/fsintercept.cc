a1612486e8741a8aed3ccf04e39f8da9bec87019
Dan Iter <daniter@stanford.edu>
2017-04-27 19:35:28 -0700

    WIP on intercepting open calls

--- src/compiler/fsintercept.cc
+++ src/compiler/fsintercept.cc
@@ -1,12 +1,44 @@
 #include <dlfcn.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "thunk.hh"
+#include "thunk_writer.hh"
 
 typedef int ( *orig_open_f_type )( const char *pathname, int flags );
- 
+
+using namespace std;
+
+const size_t PATH_MAX = 512;
+
+string get_thunk_filename() {
+  char cmdline[PATH_MAX];
+  ssize_t len = readlink("/proc/self/cmdline", cmdline, sizeof(cmdline));
+  if (len == -1 || len == sizeof(cmdline))
+    len = 0;
+  cmdline[len] = '\0';
+  cout << cmdline << endl;
+  return string(cmdline);
+}
+
+Thunk get_current_thunk() {
+  string curr_thunk_name = get_thunk_filename();
+
+  return ThunkWriter::read_thunk( curr_thunk_name );
+}
+
 extern "C" int open( const char *pathname, int flags, ... )
 {
   orig_open_f_type orig_open;
   orig_open = (orig_open_f_type)dlsym( RTLD_NEXT, "open" );
   printf( "DANITER INTERCEPTING OPEN %s\n", pathname );
+
+  // Read current thunk
+  Thunk thunk = get_current_thunk();
+
+  // Get hash of pathname
+  // TODO : Should we compute the hash given the file or just look up the hash in the thunk?
+
+  // Repace pathname
+
   return orig_open( pathname, flags );
 }
--- src/thunk/thunk_writer.cc
+++ src/thunk/thunk_writer.cc
@@ -23,3 +23,24 @@ bool ThunkWriter::write_thunk( Thunk thunk )
 
   return true;
 }
+
+Thunk ThunkWriter::read_thunk( string filename ) {
+  fstream file( filename );
+  json::Object jobj;
+  json::Reader::Read(jobj, file);
+
+  const json::String& jstr = jobj["outfile"];
+  string outfile = jstr.Value();
+  vector<string> cmd;
+  json::Array& array = jobj["args"];
+  json::Array::const_iterator it(array.Begin()), itend(array.End());
+  for (; it != itend; ++it){
+    json::String& s = *it;
+    string st = s.Value();
+    cmd.push_back( st );
+  }
+  ThunkFunc thunkfunc(cmd);
+  InFileDescriptor infile( jobj[ "infiles" ][ 0 ][ "filename" ].Value() );
+  vector<InFileDescriptor> infiles = {infile};
+  return Thunk(outfile, thunkfunc, infiles);
+}
